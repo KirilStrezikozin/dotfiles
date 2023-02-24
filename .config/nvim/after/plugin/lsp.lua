151f2919e39c9f727047dcec0a4b83d6fb764e8e
KirilStrezikozin <kirilstrezikozin@gmail.com>
2023-02-24 10:38:24 +0200

    add: show nvim lsp diagnostics

--- .config/nvim/after/plugin/lsp.lua
+++ .config/nvim/after/plugin/lsp.lua
@@ -11,3 +11,12 @@ lsp.ensure_installed({
 lsp.nvim_workspace()
 
 lsp.setup()
+
+vim.diagnostic.config({
+  virtual_text = false
+})
+
+-- Show line diagnostics automatically in hover window
+vim.o.updatetime = 250
+vim.cmd [[autocmd CursorHold,CursorHoldI * lua vim.diagnostic.open_float(nil, {focus=false})]]
+
